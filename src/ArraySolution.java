8e04c8d4675d975fbc9027d764535b97cd60e566
WangYin <13162199852@163.com>
2018-09-26 16:00:24 +0800

    maxArea

--- src/ArraySolution.java
+++ src/ArraySolution.java
@@ -7,7 +7,6 @@ public class ArraySolution {
     //    There are two sorted arrays nums1 and nums2 of size m and n respectively.
     //    Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
     //    You may assume nums1 and nums2 cannot be both empty.
-
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] num = new int[nums1.length + nums2.length];
         int j = 0, k = 0;
@@ -35,5 +34,9 @@ public class ArraySolution {
         } else {
             return num[num.length / 2];
         }
+    }
+    // Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+    public static int maxArea(int[] height) {
+
     }
 }

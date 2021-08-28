cb9874064abea9f4aa6e5b25a940f9917f604a1d
wangyin <13162199852@163.com>
2021-08-28 12:43:00 +0800

    add linkedList QA

--- Leetcode/src/Structure/LinkedList/mergeTwoLists.java
+++ Leetcode/src/Structure/LinkedList/mergeTwoLists.java
@@ -1,5 +1,7 @@
 package Structure.LinkedList;
 
+import Structure.ListNode;
+
 /**
  * @Author Wang Yin
  * @Date 2021/8/15 11:14
@@ -9,4 +11,28 @@ package Structure.LinkedList;
  * 输出：[1,1,2,3,4,4]
  */
 public class mergeTwoLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        if (l1 != null) {
+            tail.next = l1;
+        }
+        if (l2 != null) {
+            tail.next = l2;
+        }
+        return head.next;
+    }
 }
+
+

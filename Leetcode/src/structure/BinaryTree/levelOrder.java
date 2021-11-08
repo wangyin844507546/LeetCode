2982f8cdec1de4fae51da3a560a7c947cd23a029
WangYin <13162199852@163.com>
2021-11-08 19:45:49 +0800

    BSF binaryTreeTraversal

--- Leetcode/src/structure/BinaryTree/levelOrder.java
+++ Leetcode/src/structure/BinaryTree/levelOrder.java
@@ -8,6 +8,31 @@ import java.util.List;
 /**
  * @author Wang Yin * @date 2021/9/17 19:07
  *  二叉树的层序遍历
+ *  DFS 与 BFS
+ *  DFS 遍历使用递归：
+ *  void dfs(TreeNode root) {
+ *     if (root == null) {
+ *         return;
+ *     }
+ *     dfs(root.left);
+ *     dfs(root.right);
+ * }
+ *
+ * BFS 遍历使用队列数据结构：
+ * void bfs(TreeNode root) {
+ *     Queue<TreeNode> queue = new ArrayDeque<>();
+ *     queue.add(root);
+ *     while (!queue.isEmpty()) {
+ *         TreeNode node = queue.poll(); // Java 的 pop 写作 poll()
+ *         if (node.left != null) {
+ *             queue.add(node.left);
+ *         }
+ *         if (node.right != null) {
+ *             queue.add(node.right);
+ *         }
+ *     }
+ * }
+ *
  */
 public class levelOrder {
 

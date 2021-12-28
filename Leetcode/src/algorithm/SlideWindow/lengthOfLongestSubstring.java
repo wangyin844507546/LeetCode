1be64debb266c1350d03186985a4f0d50570a0a5
wangyin <13162199852@163.com>
2021-12-28 23:24:30 +0800

    SlideWindow

--- Leetcode/src/algorithm/SlideWindow/lengthOfLongestSubstring.java
+++ Leetcode/src/algorithm/SlideWindow/lengthOfLongestSubstring.java
@@ -1,11 +1,54 @@
 package algorithm.SlideWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author Wang Yin
  * @Date 2021/9/24 9:30
- *  无重复字符的最长子串
- *  给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+ *  76. 最小覆盖子串
+ *  给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。
+ *  如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+ *  输入：s = "ADOBECODEBANC", t = "ABC"
+ *  输出："BANC"
  */
 public class lengthOfLongestSubstring {
 
+    public static void main(String[] args) {
+        minWindow("a","a");
+    }
+
+    public static String minWindow(String s, String t) {
+        Map<Character,Integer> need = new HashMap<>();
+        Map<Character,Integer> window = new HashMap<>();
+        for(int i = 0; i < t.length(); i++) {
+            need.put(t.charAt(i),need.getOrDefault(t.charAt(i),0) + 1);
+        }
+        int left = 0, right = 0;
+        int valid = 0;
+        int start = 0,len = Integer.MAX_VALUE;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            if (need.containsKey(c)) {
+                window.put(c,window.getOrDefault(c,0)+1);
+                if (window.get(c).equals(need.get(c)))
+                    valid++;
+            }
+            while (valid == need.keySet().size()) {
+                if (right - left < len) {
+                    start = left;
+                    len = right - left;
+                }
+                char d = s.charAt(left);
+                left++;
+                if (need.containsKey(d)) {
+                    if (window.get(d).equals(need.get(d)))
+                        valid--;
+                    window.put(d,window.get(d)-1);
+                }
+            }
+        }
+        return len == Integer.MAX_VALUE ? "":s.substring(start,start+len);
+    }
 }
